c0f9f416ce45bf4d5f97c88a1ca65bd95767bafb
chongren zhao <528583777@gmail.com>
2026-07-14 11:09:52 +0800

    complete proj01 A1

--- Project/proj1/imageloader.c
+++ Project/proj1/imageloader.c
@@ -24,18 +24,78 @@
 //You may find the function fscanf useful.
 //Make sure that you close the file with fclose before returning.
 Image *readData(char *filename) 
-{
-	//YOUR CODE HERE
+{	FILE *fp = fopen(filename,"r");
+	if(fp == NULL) {
+		exit(-1);
+	}
+	Image* img = malloc(sizeof(Image));
+	if(img == NULL){
+		fclose(fp);
+		exit(-1);
+	}
+
+	char type[3];
+	fscanf(fp,"%2s",type);
+	int cols,rows;
+	fscanf(fp,"%d %d",&cols,&rows);
+	int maxSize;
+	fscanf(fp,"%d",&maxSize);
+
+	img->cols = cols;
+	img->rows = rows;
+	img->image = malloc(cols*rows*sizeof(Color*));
+	if(img->image == NULL){
+		free(img);
+		fclose(fp);
+		exit(-1);
+	}
+
+	for(int i = 0;i < rows; ++i){
+		for(int j = 0;j < cols; ++j){
+			int idx = i*cols + j;
+			img->image[idx] = malloc(sizeof(Color));
+			int r,g,b;
+			fscanf(fp,"%d %d %d",&r,&g,&b);
+			img->image[idx]->R = r; 
+			img->image[idx]->G = g;
+			img->image[idx]->B = b;
+		}	
+	}
+	fclose(fp);
+	return img;
 }
 
 //Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
 void writeData(Image *image)
 {
-	//YOUR CODE HERE
+	printf("P3\n");
+	printf("%d %d\n",(int)image->cols,(int)image->rows);
+	printf("255\n");
+
+	for(int idx = 0;idx < image->cols*image->rows; ++idx){
+		int row = idx/image->cols;
+		int col = idx%image->cols;
+		
+		uint8_t r,g,b;
+		r = image->image[idx]->R;
+		g = image->image[idx]->G;
+		b = image->image[idx]->B;
+
+		printf("%3d %3d %3d",(int)r,(int)g,(int)b);
+		if(col == image->cols - 1){
+			printf("\n");
+		}else{
+			printf("   ");
+		}
+	}
 }
 
 //Frees an image
 void freeImage(Image *image)
 {
-	//YOUR CODE HERE
+	for(int idx = 0;idx < image->cols*image->rows; ++idx){
+		free(image->image[idx]);
+	}
+	free(image->image);
+	free(image);
 }
